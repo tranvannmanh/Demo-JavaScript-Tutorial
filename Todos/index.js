903a30dc8a03ec6e81d5135aed97ac46e7cb35c6
tranvanmanh <manh.tranvann@gmail.com>
2022-03-20 10:51:37 +0700

    add edit_item action

--- Todos/index.js
+++ Todos/index.js
@@ -5,15 +5,18 @@ if (localStorage.getItem('todoList'))
     todoList = JSON.parse(localStorage.getItem('todoList'));
 else
     todoList = [];
-console.log(todoList);
+// console.log(todoList);
 
 const list = document.getElementById('list');
 // let items = "";
 function showTodos(todoList){
     let items =""
     for (const item of todoList) {
-        items +=  '<li>'+'<div><input name="todo_item" type="checkbox" class="items">'+item+ '</div><div class="actions_item"><button class="edit_item" onclick="return edit_item()"><i class="fa-solid fa-pen"></i></button><button class="remove_item" onclick="return delete_one(this)"><i class="fa-solid fa-xmark"></i></button></div></li>';
+        items +=  '<li>'+'<div><input name="todo_item" type="checkbox" class="items">'+item+ '</div><div class="actions_item"><button class="edit_item" onclick="return edit_item(this)"><i class="fa-solid fa-pen"></i></button><button class="remove_item" onclick="return delete_one(this)"><i class="fa-solid fa-xmark"></i></button></div></li>';
     }
+    // push to local storage
+    localStorage.setItem('todoList', JSON.stringify(todoList));
+    console.log(todoList);
     list.innerHTML=items;
 }
 showTodos(todoList);
@@ -26,16 +29,13 @@ function add_new(){
         showTodos(todoList);
         input_task.value="";
     }
-
-    // Push to local storage
-    localStorage.setItem('todoList', JSON.stringify(todoList));
 }
         
-        // Delete a specified task
+// Delete a specified task
 function delete_one(obj){
     // console.log(obj);
     // take closest li tag
-    var li = obj.closest('li');
+    const li = obj.closest('li');
     // console.log(li);
     // take array from li tags
     const nodes = Array.from(li.closest('ul').children);
@@ -43,14 +43,21 @@ function delete_one(obj){
     // remove an specified element
     todoList.splice(nodes.indexOf(li),1);
 
-
     showTodos(todoList);
-    // push to local
-    localStorage.setItem('todoList', JSON.stringify(todoList));
 }
 
 // Fix an item
-function fix_this(obj){
+function edit_item(obj){
+    const currentItem = obj.closest('li');
+    const nodes = Array.from(currentItem.closest('ul').children);
 
+    let fixed = prompt('Edit your task...');
+    if (fixed == null || fixed.trim() == "") {
+        alert('No change...');
+    }
+    else {
+        todoList[nodes.indexOf(currentItem)] = fixed;
+        showTodos(todoList);
+    }
 }
         
