ae28e68578aa920c7338e771ba1fea24902e6a83
tranvanmanh <manh.tranvann@gmail.com>
2022-03-22 16:10:40 +0700

    DOM vs VDOM

---
+++ DOMvsVDOM/DOM_VDOM.js
@@ -0,0 +1,11 @@
+/**
+ * DOM là viết tắt của Document Object Model 
+ * Mô hình tài liệu biểu diễn dưới dạng đối tượng
+ * là một nền tảng và ngôn ngữ trung gian cho phép chương trình và và các lệnh truy cập, cập nhật nội dung, cấu trúc và kiểu của tài liệu
+ * DOM gồm 3 thành phần: element, attribute, text
+ * 1. element: các thẻ như <a>, <html>, ...
+ * 2. attribute: các thuộc tính định nghĩa element -> href, src,...
+ * 3. text
+ * 
+ * HTML DOM 
+ */
